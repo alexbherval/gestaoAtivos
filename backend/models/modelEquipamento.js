470bf4e58258494f969eccc377a168963c7cb8a8
Alex Boges <87436065+alexbherval@users.noreply.github.com>
2023-10-24 10:30:40 -0300

    Criação do modelequipamento

--- backend/models/modelEquipamento.js
+++ backend/models/modelEquipamento.js
@@ -1,43 +1,46 @@
 const mongoose = require("mongoose");
 
 const EquipamentoSchema = new mongoose.Schema({
-    tipo: {
-        type: String
+    tipo: { type: String, required: true },
+    serial: { type: String, required: true },
+    productName: { type: String }, 
+    marca: { type: String, required: true },
+    modelName: { type: String },
+    dataCompra: { type: Date },
+    dataInstalacao: { type: Date },
+    statusEquipamento: { type: String },
+    nfeEntrada: {
+        dataCompra: { type: Date },
+        numeroNota: { type: String },
+        chave: { type: String },
+        vTotal: { type: Number, required },
+        urlNfe: { type: String }
     },
-    marca: {
-        type: String,
-        required: true
-    },
-    serial: {
-        type: String,
-        required: true
-    },
-    partNumber: {
-        type: String,
-        required: true
-    },
-    productName: {
-        type: String,
-        required: true
-    }, 
-    modelName: {
-        type: String,
-        required: true
-    },
-    nfe: {
-        type: String,
-        required: true
-    },
-    valorCompra: {
-        type: String,
-        required: true
-    },
-    dataCompra: {
-        type: Date
-        
-    }
+    especificacoes: {
+        // campos para computadores e notebooks
+        processador: { 
+            marca: { type: String },
+            frequencia: { type: String },
+            cache: { type: String }
+        },
+        memoria: {
+            tamanho: { type: String },
+            frequencia: { type: String }
+        },
+        Hdd: {
+            capacidade: { type: String },
+            tipo: { type: String }
+        },
+        fonte: {
+            potencia: {type: String },
+            voltagem: { type: String },
+            padrao:{ type: String }
+        },
+        // campos para monitores
+        polegadas: { type: String },
+    },     
 });
 
-const User = mongoose.model('Equipamento', UserSchema);
+const User = mongoose.model('Equipamento', EquipamentoSchema);
 
 module.exports = Equipamento;
